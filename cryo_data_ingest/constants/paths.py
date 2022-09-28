9bad5122a5a23a74f5652495c5877be87e95f1ec
Matt Fisher <matt.fisher@nsidc.org>
2022-09-28 16:40:12 -0600

    Init static analysis config

--- cryo_data_ingest/constants/paths.py
+++ cryo_data_ingest/constants/paths.py
@@ -1,4 +1,8 @@
 from pathlib import Path
 
 
+PACKAGE_DIR = Path(__file__).parent.parent
+PROJECT_DIR = PACKAGE_DIR.parent
+SCRIPTS_DIR = PROJECT_DIR / 'scripts'
+
 JSON_STORAGE_DIR = Path('/tmp/cryo-data-ingest')
